f9ae57557c58c4aa91399dd847d58dbc29b7041a
Hojoong Yoon <932400976@bun2-206-15.local>
2019-03-13 14:20:35 +1300

    initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const http =require('http');
+const fs =require('fs');
+const path =require('path');
+const data =require('data');
+const qs =require('querystring'); // read the url with the query string
